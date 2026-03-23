792ba16336c014a76db064f3748dc863f3567342
sauravnirala <sauravnirala44@gmail.com>
2026-03-24 01:44:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running");
 });
